eb61125aeec34ee24ee09e0758d0c849057ecabf
Habibabelhadghamouda <bhh.habiba12@gmail.com>
2025-03-17 22:43:07 +0100

    index.js

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const App = express();
+const port = 5000;
+const cors = require("cors");
+App.use(express.json());
+App.use(cors());
+
+App.use();
+App.use()
+
+App.listen(port, () => {
+  console.log(  `App listening on http://127.0.0.1:${port}`)  ;
+});
