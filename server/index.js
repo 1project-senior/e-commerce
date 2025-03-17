7a6b8402cc5abe9da435b281b2bc3e8f9d9884f2
Jemai-Mohamed <medjemai4go@gmail.com>
2025-03-17 23:00:05 +0100

    fixing some files

--- server/index.js
+++ server/index.js
@@ -5,8 +5,8 @@ const cors = require("cors");
 App.use(express.json());
 App.use(cors());
 
-App.use();
-App.use()
+// App.use();
+// App.use()
 
 App.listen(port, () => {
   console.log(  `App listening on http://127.0.0.1:${port}`)  ;
