1e437b3c36d29e72557f82a35a621e4a44c62270
Jemai-Mohamed <medjemai4go@gmail.com>
2025-03-25 10:39:47 +0100

    pulling habiba's tasks

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { QueryClient, QueryClientProvider  } from '@tanstack/react-query'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Layout from "./Layout/Layout.jsx";
 import Navbar from "./Components/Navbar.jsx";
 import Categories from "./Components/Categories.jsx";
@@ -11,35 +11,25 @@ import Cart from "./Components/Cart.jsx";
 import AddProduct from "./Components/AddProduct.jsx";
 import ProductDetails from "./Components/ProductDetails.jsx";
 
-
-
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 function App() {
- 
   return (
-    <QueryClientProvider  client={queryClient}>
+    <QueryClientProvider client={queryClient}>
       <BrowserRouter>
         <Navbar />
         <Routes>
           <Route element={<Layout />}>
             <Route path="/" element={<Home />} />
             <Route path="/Categories" element={<Categories />} />
-            <Route
-              path="/Shop-page"
-              element={<Shop  />}
-            />
+            <Route path="/Shop-page" element={<Shop />} />
             <Route path="/Blog" element={<Blog />} />
             <Route path="/Cart" element={<Cart />} />
             <Route path="/Add-product" element={<AddProduct />} />
-            <Route
-              path="/Shop-page/:id"
-              element={<ProductDetails  />}
-            />
+            <Route path="/Shop-page/:id" element={<ProductDetails />} />
           </Route>
         </Routes>
       </BrowserRouter>
-     
     </QueryClientProvider>
   );
 }
--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -138,3 +138,4 @@ const HomePage = () => {
 };
 
 export default HomePage;
+
